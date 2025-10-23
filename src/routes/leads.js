227c5972f4660c9d9cd1d4df51ce8d1de29bf159
Tomas Monteiro <tomasthetrader@gmail.com>
2025-10-23 14:15:50 +0100

    add leads table

--- src/routes/leads.js
+++ src/routes/leads.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { fn, col, where } = require("sequelize");
-const { User } = require("../models");
-const { Lead } = require("../models");
+const { User, Lead } = require("../../models");
 
 const router = express.Router();
 
